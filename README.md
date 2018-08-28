df76e497cadaf5323431f81f5d2d3fe2eea854e4
Georges Labrèche <georges@opendatakosovo.org>
2018-08-28 15:00:16 +0200

    generating imagery URLs that work.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# eo-browser-tile-url-generator
-Generate a CSV file listing tile URL tasks to import into a PyBossa instance.
+# PyBossa EO Browser Imagery Task Generator
+Generate a CSV file listing imagery URLs. The CSV file is to be imported into a [PyBossa](https://pybossa.com/) instance for crowdsourcing purposes.  Each row in the CSV files is associated to a microtask.
+
+This particular script was developed for a crowdsourcing project hosted in [crowdcrafting.org](https://crowdcrafting.org/).
+
+The list of imagery URLs are generated based on the following inputs:
+
+ - Bounding box of the project's area of interest (latitude and longitude bounds).
+ - Imagery dimensions for a task (i.e. x and y lengths)
--- run.py
+++ run.py
@@ -2,34 +2,22 @@ from pyproj import Proj, transform
 import csv
 import furl
 
-x_dimension = 1600
-y_dimension = 800
-resolution = 25 # meters.
+wms_service_user_id = ''
+wms_url = 'https://services.sentinel-hub.com/ogc/wms/' + wms_service_user_id
+
+x_dimension = 1600 # meters.
+y_dimension = 800 # meters.
 
 lat_bounds = [51.72, 52.22]
 lng_bounds = [-2.28, -1.21]
 
 # Upper left hand corner coords.
-ulhc_coords = [lat_bounds[1], lng_bounds[0]]
+ulhc_coords = [lng_bounds[0], lat_bounds[1]]
 
 # Lower right hand corner coords.
-lrhc_coords = [lat_bounds[0], lng_bounds[1]]
-
-#lat_step = 0.01
-#lng_step = 0.02
+lrhc_coords = [lng_bounds[1], lat_bounds[0]]
 
 times = ['2018-05-15', '2018-06-26', '2018-06-29', '2018-07-06']
-
-#zoom = 16
-
-#preset = '1_TRUE_COLOR'
-#data_source = 'Sentinel-2%20L2A'
-
-#url_template = 'https://apps.sentinel-hub.com/eo-browser/?lat={}&lng={}&zoom={}&time=TIME_TOKEN&preset={}&datasource={}'
-
-api_key = ''
-
-url = 'http://services.sentinel-hub.com/ogc/wms/' + api_key
 
 params = {
     'service': 'WMS',
@@ -43,98 +31,89 @@ params = {
     'height': y_dimension,
     'width': x_dimension,
     'maxcc': 1,
-    'time': times[0],
-    'srs': 'EPSG%3A3857', 
-    'bbox': '{},{},{},{}' #xmin, ymin, xmax, ymax.
+    'time': 'TIME_TOKEN',
+    'srs': 'EPSG%3A3857' 
 }
 
-
-f = furl.furl('')
-
+furl = furl.furl('')
 
 def main():
 
+    # Define projections.
     in_proj = Proj(init='epsg:4326')
     out_proj = Proj(init='epsg:3857')
 
+    # Get x and y coordinates for upper left hand corner and lower right hand corner coords. 
     ulhc_x, ulhc_y = transform(in_proj, out_proj, ulhc_coords[0], ulhc_coords[1])
-    #print ulhc_x, ulhc_y
-
     lrhc_x, lrhc_y = transform(in_proj, out_proj, lrhc_coords[0], lrhc_coords[1])
-    #print lrhc_x, lrhc_y
 
-    # Init the bounds for the first task (on the upper left hand corner of the AOI).
-    x_bounds_current = [int(ulhc_x), int(ulhc_x - x_dimension)]
-    y_bounds_current = [int(ulhc_y), int(ulhc_y + y_dimension)]
-
-    # Redefining bounds so that we traverse the AOI row by row.
-    while y_bounds_current[1] <= lrhc_y:
-        while x_bounds_current[1] >= lrhc_x:
-        
-            # Shift to the next x bounds
-            x_bounds_current = [x_bounds_current[1], x_bounds_current[1] - x_dimension]
-
-            #print x_bounds_current
-            #print y_bounds_current
-
-            params['bbox'] = params['bbox'].format(
-                x_bounds_current[0], y_bounds_current[0],
-                x_bounds_current[1], y_bounds_current[1])
-
-            f.args = params
-
-            print url + f.url
-            print
-            break
-        break
-
-        # Completed the row on the x axis:
-        # 1. Shift the y bounds to cover the next row.
-        y_bounds_current = [y_bounds_current[1], y_bounds_current[1] + y_dimension]
-
-        # 2. Shift the x bounds back to the beginning for the new row.
-        x_bounds_current = [int(ulhc_x), int(ulhc_x - x_dimension)]
-
-
-    #print ''
-    #print ulhc_x, ulhc_y
-    #print lrhc_x, lrhc_y
-
-    '''
-    field_names = ['img_url', 'lat', 'lng', 'zoom', 'time1', 'time2', 'time3', 'time4']
+    
+    # Open CSV file to write task rows into.
     with open('tasks.csv', mode='w') as tasks_csv_file:
 
-        # Create CSV Writer and writer header.
+        # Create CSV Writer and write header.
+        field_names = ['img_url', 'xmin', 'ymin', 'xmax', 'ymax', 'lat', 'lng', 'time1', 'time2', 'time3', 'time4']
         writer = csv.DictWriter(tasks_csv_file, fieldnames=field_names)
         writer.writeheader()
-        
-        # Initialize lower bounds.
-        lat = lat_bounds[0]
-        lng = lng_bounds[0]
 
-        # Write task rows.
-        while lat <= lat_bounds[1]:
-            while lng <= lng_bounds[1]:
+        # Init the bounds for the first task (on the upper left hand corner of the AOI).
+        x_bounds_current = [int(ulhc_x), int(ulhc_x + x_dimension)]
+        y_bounds_current = [int(ulhc_y), int(ulhc_y - y_dimension)]
+
+        # Task counter
+        task_counter = 0
+
+        # Redefining bounds so that we traverse the AOI row by row.
+        while y_bounds_current[1] >= lrhc_y:
+            while x_bounds_current[1] <= lrhc_x:
+
+                x_min = x_bounds_current[0]
+                x_max = x_bounds_current[1]
+                y_min = y_bounds_current[0]
+                y_max = y_bounds_current[1]
+                x_center = (x_min + x_max) / 2
+                y_center = (y_min + y_max) / 2
+
+                # Define bounding box's xmin, ymin, xmax, and ymax values.
+                params['bbox'] = '{},{},{},{}'.format(
+                    x_min, y_min,
+                    x_max, y_max)
+
+                # Get center lat/lng coordinates
+                lng, lat = transform(out_proj, in_proj, x_center, y_center)
 
                 # Build image url.
-                img_url = url_template.format(lat, lng, zoom, preset, data_source)
+                furl.args = params
+                img_url = wms_url + furl.url
 
                 # Write task row in CSV file.     
                 writer.writerow({
                     'img_url': img_url, 
+                    'xmin': x_min,
+                    'ymin': y_min,
+                    'xmax': x_max,
+                    'ymax': y_max,
                     'lat': lat,
                     'lng': lng,
-                    'zoom': zoom,
                     'time1': times[0],
                     'time2': times[1],
                     'time3': times[2],
                     'time4': times[3]})
 
-                lng = lng + lng_step
+                # Shift to the next x bounds
+                x_bounds_current = [x_max, x_max + x_dimension]
 
-            lat = lat + lat_step
-            lng = lng_bounds[0]
-    '''
+                task_counter = task_counter + 1
+            
+            # Completed the row on the x axis:
+            # 1. Shift the y bounds to cover the next row.
+            y_bounds_current = [y_max, y_max - y_dimension]
+
+            # 2. Shift the x bounds back to the beginning for the new row.
+            x_bounds_current = [int(ulhc_x), int(ulhc_x + x_dimension)]
+
+    print 'Total tasks created: ' + str(task_counter)
+
 
 if __name__ == "__main__":
     main()
